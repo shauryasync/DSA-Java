ce3ed67b1a9abb1ff03034e819614681fe671db7
Shaurya Pratap Singh <edshaurya@gmail.com>
2025-12-08 14:33:36 +0530

    Day03: Rotate array elements solution
    Implement a method to rotate an array by one position.

---
+++ Day03/rotateByOne.java
@@ -0,0 +1,16 @@
+public class rotateByOne {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        int last = arr[arr.length - 1];
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = last;
+
+        for (int v : arr) {
+            System.out.print(v + " ");
+        }
+    }
+}
