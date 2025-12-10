e5d3ffe8673679628220e2d321a47fb4c301c046
Shaurya Pratap Singh <edshaurya@gmail.com>
2025-12-10 14:09:10 +0530

    Day06: subarray printing for an integer array

---
+++ Day06/subArrayofArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class subArrayofArray {
+    public static void main(String[] args) {
+      int [] arr = {1,2,3};
+      int n = arr.length;
+      for(int i = 0; i < n; i++ ){
+        for(int j = i; j < n; j++){
+          for(int k = i; k <=j; k++){
+            System.out.print(arr[k]+" ");
+          }
+          System.out.println();
+        }
+      }
+  }
+}
